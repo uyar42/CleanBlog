913565b2645eeca45353526c7ddcdc2f50a078b6
mehmet <71780605+uyar42@users.noreply.github.com>
2023-05-17 15:35:31 +0300

    1.Bölüm

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.get('/', (req, res) => {
+  res.send(blog);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

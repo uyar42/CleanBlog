28d0d4e140c1e3d581df96edf7c9493214f05f41
mehmet <71780605+uyar42@users.noreply.github.com>
2023-05-18 16:22:48 +0300

    ejs , partials

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = 3000;
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
 app.use(express.static('public'));
 
 const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(port, () => {

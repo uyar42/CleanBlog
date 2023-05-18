c5e53cc651df99cef0d8d1fbe855e359d6cf0b40
mehmet <71780605+uyar42@users.noreply.github.com>
2023-05-18 15:20:00 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 app.get('/', (req, res) => {
   res.send(blog);
